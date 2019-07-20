f35b25fe87d17b2ead6ea5989f6642e88acff3e1
Abdul Mobeen <mobeen@alkunat.com>
2019-07-20 16:08:01 +0200

    videos list -v3 -- searching

--- src/Controller/FrontController.php
+++ src/Controller/FrontController.php
@@ -12,18 +12,18 @@ use Symfony\Component\HttpFoundation\Request;
 class FrontController extends AbstractController
 {
     /**
-    * @Route("/", name="main_page")
-    */
+     * @Route("/", name="main_page")
+     */
     public function index()
     {
         return $this->render('front/index.html.twig');
     }
-    
+
 
     /**
-    * @Route("/video-list/category/{categoryname},{id}/{page}", defaults={"page": "1"}, name="video_list")
-    */
-    public function videoList($id, $page, CategoryTreeFrontPage $categories,Request $request)
+     * @Route("/video-list/category/{categoryname},{id}/{page}", defaults={"page": "1"}, name="video_list")
+     */
+    public function videoList($id, $page, CategoryTreeFrontPage $categories, Request $request)
     {
 
         $ids = $categories->getChildIds($id);
@@ -31,74 +31,89 @@ class FrontController extends AbstractController
 
         $videos = $this->getDoctrine()
             ->getRepository(Video::class)
-            ->findByChildIds($ids ,$page, $request->get('sortby'));
+            ->findByChildIds($ids, $page, $request->get('sortby'));
 //        ->findByChildIds($ids ,$page);
 
 
         $categories->getCategoryListAndParent($id);
-        return $this->render('front/video_list.html.twig',[
+        return $this->render('front/video_list.html.twig', [
             'subcategories' => $categories,
-            'videos'=>$videos
+            'videos' => $videos
         ]);
     }
-        
-        /**
-        * @Route("/video-details", name="video_details")
-        */
-        public function videoDetails()
-        {
-            return $this->render('front/video_details.html.twig');
+
+    /**
+     * @Route("/video-details", name="video_details")
+     */
+    public function videoDetails()
+    {
+        return $this->render('front/video_details.html.twig');
+    }
+
+    /**
+     * @Route("/search-results/{page}", methods={"GET"}, defaults={"page": "1"}, name="search_results")
+     */
+    public function searchResults($page, Request $request)
+    {
+        $videos = null;
+        $query = null;
+
+        if ($query = $request->get('query')) {
+            $videos = $this->getDoctrine()
+                ->getRepository(Video::class)
+                ->findByTitle($query, $page, $request->get('sortby'));
+
+            if (!$videos->getItems()) $videos = null;
         }
-        
-        /**
-        * @Route("/search-results", methods={"POST"}, name="search_results")
-        */
-        public function searchResults()
-        {
-            return $this->render('front/search_results.html.twig');
-        }
-        
-        /**
-        * @Route("/pricing", name="pricing")
-        */
-        public function pricing()
-        {
-            return $this->render('front/pricing.html.twig');
-        }
-        
-        /**
-        * @Route("/register", name="register")
-        */
-        public function register()
-        {
-            return $this->render('front/register.html.twig');
-        }
-        
-        /**
-        * @Route("/login", name="login")
-        */
-        public function login()
-        {
-            return $this->render('front/login.html.twig');
-        }
-        
-        /**
-        * @Route("/payment", name="payment")
-        */
-        public function payment()
-        {
-            return $this->render('front/payment.html.twig');
-        }
-        
-        public function mainCategories()
-        {
-            $categories = $this->getDoctrine()
+
+        return $this->render('front/search_results.html.twig', [
+            'videos' => $videos,
+            'query' => $query,
+        ]);
+    }
+
+
+    /**
+     * @Route("/pricing", name="pricing")
+     */
+    public function pricing()
+    {
+        return $this->render('front/pricing.html.twig');
+    }
+
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register()
+    {
+        return $this->render('front/register.html.twig');
+    }
+
+    /**
+     * @Route("/login", name="login")
+     */
+    public function login()
+    {
+        return $this->render('front/login.html.twig');
+    }
+
+    /**
+     * @Route("/payment", name="payment")
+     */
+    public function payment()
+    {
+        return $this->render('front/payment.html.twig');
+    }
+
+    public function mainCategories()
+    {
+        $categories = $this->getDoctrine()
             ->getRepository(Category::class)
-            ->findBy(['parent'=>null], ['name'=>'ASC']);
-            return $this->render('front/_main_categories.html.twig',[
-                'categories'=>$categories
-                ]);
-            }
-        }
+            ->findBy(['parent' => null], ['name' => 'ASC']);
+        return $this->render('front/_main_categories.html.twig', [
+            'categories' => $categories
+        ]);
+    }
+}
         
         
